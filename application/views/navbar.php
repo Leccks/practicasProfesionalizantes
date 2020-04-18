3a17415e30f825a099bcb925890a422737e99856
Rabeirou <franciscorabito16@gmail.com>
2020-04-17 22:00:40 -0300

    agregamos carpeta assets/css

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,33 +1,4 @@
-<style>
-    .lightgrey{
-        background-color: lightgrey;
-    }
-    .center{
-        align-items: center;
-        justify-content: center;
-    }
-    .dflex{
-        display: flex;
-    }
-    .w100{
-        width: 100%;
-    }
-    .w10{
-        width: 10%;
-    }
-    .w2666{
-        width: 26.66%;
-    }
-    .h10{
-        height: 10%;
-    }
-    .h100{
-        height: 100%;
-    }
-    .dirrow{
-        flex_direction: row;
-    }
-</style>
+<link rel="stylesheet" href="../assets/css/navbar.css">
 <navbar class="dflex w100 h10 dirrow lightgrey">
     <button class="h100 dflex w10 center">Menu desplegable</button>
     <a href="http://localhost/practicasProfesionalizantes/" class="h100 dflex w2666 center">Inicio</a>
