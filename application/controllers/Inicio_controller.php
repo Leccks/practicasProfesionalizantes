a91b00a92d685579394abfa53b1e5945f19b9469
Leccks <palomierilucas@gmail.com>
2020-04-17 22:54:02 -0300

    Funciona geturl() y se puede ir a login

--- application/controllers/Inicio_controller.php
+++ application/controllers/Inicio_controller.php
@@ -27,6 +27,6 @@ class Inicio_controller extends CI_Controller {
 	public function index(){	
 		$data['url'] = url();
 		$this->load->view('navbar',$data);
-		$this->load->view('inicio', $data);
+		$this->load->view('inicio',$data);
 	}
 }
